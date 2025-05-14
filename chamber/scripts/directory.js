3aacfe1a119b82ce3ade6cc6b6927524768d1b09
josephats1 <bjerrykibs@gmail.com>
2025-05-14 20:53:12 +0300

    removed link

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -148,3 +148,10 @@ async function fetchBusinessData() {
 
 // Initialize the application
 fetchBusinessData();
+//Load Font Awesome after validation
+window.addEventListener('DOMContentLoaded', function() {
+  var fa = document.createElement('link');
+  fa.rel = 'stylesheet';
+  fa.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css';
+  document.head.appendChild(fa);
+});
